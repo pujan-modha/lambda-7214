f1a2db16ac9db1a54cc0bf22b142bea54a7a13dd
pujan-modha <pujanmodha19@gmail.com>
2023-09-05 00:14:39 +0530

    minor fixes for mobile devices

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ const Home = () => (
     <section className='w-full flex-center flex-col'>
         <h1 className='head_text text-center'>
             Discover & Share
-            <br className='max-md:hidden' />
+            <br className='max-md' />
             <span className='orange_gradient text-center'> AI-Powered Prompts</span>
         </h1>
         <p className='desc text-center'>
