f2a662589f22b3693f4b442c0977d3b67719c249
pujan-modha <pujanmodha19@gmail.com>
2023-09-04 18:30:33 +0530

    optimized imports

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,11 +1,10 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import {useEffect, useState} from "react";
+import {useSession} from "next-auth/react";
+import {useRouter} from "next/navigation";
 
 import Profile from '@components/Profile';
-import {router} from "@node_modules/next/dist/client";
 
 const MyProfile = () => {
 
