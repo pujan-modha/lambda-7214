4f6a024c4f957b9faff92b6268d60c9b34db82e9
pujan-modha <pujanmodha19@gmail.com>
2023-09-05 00:11:04 +0530

    minor changes

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -72,7 +72,7 @@ const Feed = () => {
             <form className='relative w-full flex-center'>
                 <input
                     type='text'
-                    placeholder='Search for a tag or a username'
+                    placeholder='Search for a tag or a username or a prompt'
                     value={searchText}
                     onChange={handleSearchChange}
                     required
